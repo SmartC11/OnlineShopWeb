ea64e1bc2a7e9e6a6b48a8bb0a15ffe852c9fd94
Smart Cheng <1939366545@qq.com>
2024-11-22 16:26:29 +0800

    Initial commit: Set up the basic project structure 第一次提交：搭建了项目的初步结构

---
+++ src/main/java/com/gmail/chengzijian68/onlineshopweb/OnlineShopWebApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.chengzijian68.onlineshopweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopWebApplication.class, args);
+    }
+
+}
